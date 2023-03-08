c9e53e6925f83860686ecba8f3ceb2583fea2d78
k-san-official <konstantin.sandfort@gmail.com>
2023-03-08 11:08:49 +0100

    argmax test

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
+import stateFactory
+import state
 
+"""
 fashiondata = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = fashiondata.load_data()
 print(x_test.shape)
@@ -20,3 +24,17 @@ model.compile(
 
 # Train the NN
 model.fit(x_train, y_train, epochs=5)
+"""
+
+
+def get_max_index(arr):
+    max_indices = np.argmax(arr, axis=0)
+    return max_indices
+
+s2 = stateFactory.generate_complete_state()
+s1 = stateFactory.generate_invalid_box_state()
+s3 = stateFactory.generate_invalid_row_state()
+
+a = [s1.board, s2.board, s3.board]
+
+print(get_max_index(a))
