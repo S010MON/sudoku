f7ee9525de58bbc90974ac5d626d33fbad1ad0c6
S010MON <leon_debnath@hotmail.co.uk>
2023-03-09 14:31:25 +0100

    Training started

--- nn_utils.py
+++ nn_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def one_hot_encode(state: np.ndarray) -> np.ndarray:
-    return (np.arange(state.max()) == state[..., None]-1).astype(int)
+    return (np.arange(state.max()) == state[..., None]-1).astype(int).flatten()
 
 def get_max_index(arr):
     max_indices = np.argmax(arr, axis=0)
