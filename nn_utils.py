6482fea66e1cbefcf209292148531fe6b878d0fc
S010MON <leon_debnath@hotmail.co.uk>
2023-03-10 11:29:25 +0100

    Convolutional Neural Network Implementation

--- nn_utils.py
+++ nn_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def one_hot_encode(state: np.ndarray) -> np.ndarray:
-    return (np.arange(state.max()) == state[..., None]-1).astype(int).flatten()
+    return (np.arange(state.max()) == state[..., None]-1).astype(int)
 
 def get_max_index(arr):
     max_indices = np.argmax(arr, axis=0)
